ed1c6bae2ca5a59f12f4cc1ced79441e1d02a8c1
Tahir Awan <tahir.awan@gmail.com>
2020-04-06 21:04:25 +0300

    added antd and tree in /coa1.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../node_modules/ag-grid-community/dist/styles/ag-grid.css"
 import "../node_modules/ag-grid-community/dist/styles/ag-theme-blue.css"
+import 'antd/dist/antd.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
