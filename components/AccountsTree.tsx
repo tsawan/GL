919b161d1f8550c205d404f0bd38f297f578b95c
Tahir Awan <tahir.awan@gmail.com>
2020-05-06 14:28:01 +0300

    selected account and parents working

--- components/AccountsTree.tsx
+++ components/AccountsTree.tsx
@@ -1,110 +1,128 @@
-import gql from "graphql-tag";
-import { useState } from "react";
-import { Tree, Input } from "antd";
-import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag'
+import { useState } from 'react'
+import { Tree, Input } from 'antd'
+import { useQuery } from '@apollo/react-hooks'
 
-const { Search } = Input;
+const { Search } = Input
 
 // check why (query getAccounts) is not working while it's required
 // for mutations
-const query_test = gql`{
-    coa:grpglcodesl2 {
+const query_test = gql`
+  {
+    coa: grpglcodesl2 {
       key: grpglcodel2
       title
       children: grpglcodes {
-      key: grpglcode
-      title
+        key: grpglcode
+        title
+      }
     }
   }
-}`;
+`
 
-const query = gql`{
-  coa:grpglcodesl2 {
-    key: grpglcodel2
-    title
-    children: grpglcodes {
-      key: grpglcode
+const query = gql`
+  {
+    coa: grpglcodesl2 {
+      key: grpglcodel2
       title
-      children: subgrpglcodes {
-        key:subgrpglcode
+      children: grpglcodes {
+        key: grpglcode
         title
-        children: glcodes {
-          key:glcode
+        children: subgrpglcodes {
+          key: subgrpglcode
           title
+          children: glcodes {
+            key: glcode
+            title
+          }
         }
       }
     }
-}
-}`;
+  }
+`
 
-const getParentKey = (key, tree) => {
-  let parentKey;
+const getParent = (key, tree) => {
+  let parent
   for (let i = 0; i < tree.length; i++) {
-    const node = tree[i];
+    const node = tree[i]
     if (node.children) {
-      if (node.children.some(item => item.key === key)) {
-        parentKey = node.key;
-      } else if (getParentKey(key, node.children)) {
-        parentKey = getParentKey(key, node.children);
+      if (node.children.some((item) => item.key === key)) {
+        parent = { key: node.key, title: node.title }
+      } else if (getParent(key, node.children)) {
+        parent = getParent(key, node.children)
       }
     }
   }
-  return parentKey;
-};
-
-const onSelect = (selectedKeys, info) => {
-  console.log(info.node);
-};
+  return parent
+}
 
 const AccountsTree = () => {
-  const [treeData, setTreeData] = useState([]);
-  const [init, setInit] = useState(false);
-  const [searchValue, setSearchValue] = useState('');
-  const [expandedKeys, setExpandedKeys] = useState([]);
-  const [autoExpandParent, setAutoExpandParent] = useState(false);
+  const [treeData, setTreeData] = useState([])
+  const [init, setInit] = useState(false)
+  const [searchValue, setSearchValue] = useState('')
+  const [expandedKeys, setExpandedKeys] = useState([])
+  const [autoExpandParent, setAutoExpandParent] = useState(false)
   const [dataList, setDataList] = useState([])
 
-  const _list = [];
-  const generateList = data => {
+  const _list = []
+  const generateList = (data) => {
     for (let i = 0; i < data.length; i++) {
-      const node = data[i];
-      const { key } = node;
-      _list.push({ key, title: node.title });
+      const node = data[i]
+      const { key } = node
+      _list.push({ key, title: node.title })
       if (node.children) {
-        generateList(node.children);
+        generateList(node.children)
       }
     }
-  };
+  }
 
-  const onExpand = _expandedKeys => {
-    setExpandedKeys(_expandedKeys);
-    setAutoExpandParent(false);
-  };
+  const onSelect = (selectedKeys, info) => {
+    console.log(info.node)
+    const title = dataList
+      .filter((item) => item.key === info.node.key)
+      .map((node) => node.title)
+    let current: any = { key: info.node.key, title: title[0] }
+    let selected = current
+    debugger
+    while (current) {
+      let _parent = getParent(current.key, treeData)
+      if (_parent) {
+        console.log(`key: ${_parent.key}, title: ${_parent.title}`)
+        current.parent = _parent
+      }
+      current = _parent
+    }
+    console.log('==> ', selected)
+  }
 
-  const onSearch = value => {
+  const onExpand = (_expandedKeys) => {
+    setExpandedKeys(_expandedKeys)
+    setAutoExpandParent(false)
+  }
 
+  const onSearch = (value) => {
     const _expandedKeys = dataList
-      .map(item => {
+      .map((item) => {
         if (item.title.toUpperCase().indexOf(value.toUpperCase()) > -1) {
-          return getParentKey(item.key, treeData);
+          return getParent(item.key, treeData).key
         }
-        return null;
+        return null
       })
-      .filter((item, i, self) => item && self.indexOf(item) === i);
+      .filter((item, i, self) => item && self.indexOf(item) === i)
     //console.log(_expandedKeys);
-    setSearchValue(value);
-    setExpandedKeys(_expandedKeys);
-    setAutoExpandParent(true);
-  };
-  const loop = data =>
-    data.map(item => {
-      const searchLength = searchValue.length;
-      const index = item.title.toUpperCase().indexOf(searchValue.toUpperCase());
-      const beforeStr = item.title.substr(0, index);
-      const highlightStr = item.title.substr(index, searchLength);
-      const afterStr = item.title.substr(index + searchLength);
+    setSearchValue(value)
+    setExpandedKeys(_expandedKeys)
+    setAutoExpandParent(true)
+  }
+  const loop = (data) =>
+    data.map((item) => {
+      const searchLength = searchValue.length
+      const index = item.title.toUpperCase().indexOf(searchValue.toUpperCase())
+      const beforeStr = item.title.substr(0, index)
+      const highlightStr = item.title.substr(index, searchLength)
+      const afterStr = item.title.substr(index + searchLength)
       const title =
-        ((searchLength > 0) && (index > -1)) ? (
+        searchLength > 0 && index > -1 ? (
           <span>
             {beforeStr}
             <span className="site-tree-search-value">{highlightStr}</span>
@@ -112,47 +130,51 @@ const AccountsTree = () => {
           </span>
         ) : (
           <span>{item.title}</span>
-        );
+        )
       if (item.children) {
-        return { title, key: item.key, children: loop(item.children) };
+        return { title, key: item.key, children: loop(item.children) }
       }
 
       return {
         title,
         key: item.key,
-      };
-    });
-  const formatData = data => {
+      }
+    })
+  const formatData = (data) => {
     let tree = [
       {
-        key: "000000",
-        title: "Chart of Account",
-        children: data
-      }
-    ];
+        key: '000000',
+        title: 'Chart of Account',
+        children: data,
+      },
+    ]
 
     setTimeout(() => {
-      generateList(tree);
-      setDataList(_list);
-      console.warn('datalist ' + dataList.length);
-      setTreeData(tree);
-      setInit(true);
-    }, 100);
-  };
+      generateList(tree)
+      setDataList(_list)
+      console.warn('datalist ' + dataList.length)
+      setTreeData(tree)
+      setInit(true)
+    }, 100)
+  }
 
-  const { loading, data, error } = useQuery(query);
+  const { loading, data, error } = useQuery(query)
   if (loading || !data) {
-    return <div>Loading data...</div>;
+    return <div>Loading data...</div>
   }
   if (error) {
-    return <div>Error..</div>;
+    return <div>Error..</div>
   }
   if (data) {
-    if (!init) formatData(data.coa);
+    if (!init) formatData(data.coa)
     return (
       <div>
-        <Search style={{ marginBottom: 8 }} placeholder="Search" enterButton={true}
-          onSearch={onSearch} />
+        <Search
+          style={{ marginBottom: 8 }}
+          placeholder="Search"
+          enterButton={true}
+          onSearch={onSearch}
+        />
 
         <Tree
           defaultExpandAll={false}
@@ -166,9 +188,9 @@ const AccountsTree = () => {
           onSelect={onSelect}
           treeData={loop(treeData)}
         />
-        </div>
-    );
+      </div>
+    )
   }
-};
+}
 
-export default AccountsTree;
+export default AccountsTree
