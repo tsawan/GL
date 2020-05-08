e1062a343f0475c2608358c66a7dd7749fc8dc7f
Tahir Awan <tahir.awan@gmail.com>
2020-05-08 13:13:25 +0300

    refactored

--- pages/coa1.tsx
+++ pages/coa1.tsx
@@ -14,8 +14,8 @@ import { PageHeader } from 'antd'
 const coa1 = () => {
   const [values, setValues] = useState({
     mainGrpCode: '01',
-    mainGroupDesc: 'Property and AssetsXX',
-    glGroup: '0108',
+    mainGroupDesc: 'Property and Assets',
+    glGroupCode: '0108',
     glGroupDesc: 'Trade debitors',
     subGroupCode: '010802',
     subGroupDesc: 'Local debitors',
@@ -36,28 +36,21 @@ const coa1 = () => {
     const level = selection.length
     console.log(`level ${selection.length}`)
     console.log('selection ', selection)
-    // todo: improve later
+
+    // todo: move out
+    const MAX_LEVEL: number = 4
+    for (let i = level; i < MAX_LEVEL; i++)
+      selection[i] = { key: '', title: '' }
+
     let updated: any = {
       mainGrpCode: selection[0].key,
       mainGroupDesc: selection[0].title,
-      glGroup: '',
-      glGroupDesc: '',
-      subGroupCode: '',
-      subGroupDesc: '',
-      glCode: '',
-      glHead: '',
-    }
-    if (level > 1) {
-      updated.glGroup = selection[1].key
-      updated.glGroupDesc = selection[1].title
-    }
-    if (level > 2) {
-      updated.subGroupCode = selection[2].key
-      updated.subGroupDesc = selection[2].title
-    }
-    if (level > 3) {
-      updated.glCode = selection[3].key
-      updated.glHead = selection[3].title
+      glGroupCode: selection[1].key,
+      glGroupDesc: selection[1].title,
+      subGroupCode: selection[2].key,
+      subGroupDesc: selection[2].title,
+      glCode: selection[3].key,
+      glHead: selection[3].title,
     }
     setValues(updated)
   }
@@ -93,7 +86,7 @@ const coa1 = () => {
               initialValues={{
                 mainGrpCode: values.mainGrpCode,
                 mainGroupDesc: values.mainGroupDesc,
-                glGroup: values.glGroup,
+                glGroupCode: values.glGroupCode,
                 glGroupDesc: values.glGroupDesc,
                 subGroupCode: values.subGroupCode,
                 subGroupDesc: values.subGroupDesc,
@@ -140,8 +133,8 @@ const coa1 = () => {
                       />
                       <input
                         type="text"
-                        name="glGroup"
-                        value={props.values.glGroup}
+                        name="glGroupCode"
+                        value={props.values.glGroupCode}
                       />
                       <input
                         type="text"
